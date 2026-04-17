f1807cb05ad8f099019563396f8b42ad41577d76
Pedro Favela <pedro.pereira.favela@hotmail.com>
2026-04-17 13:01:53 -0300

    feat - adição de banco de dados em memória pra gerenciamento de salas

--- src/managers/RoomManager.js
+++ src/managers/RoomManager.js
@@ -1,5 +1,45 @@
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
+
+//* Super placeholder, fazendo o basico pra entender como fazer
+
+// Banco de dados em memória pra gerenciar as salas, 
+// com a tabela de jogadores nas salas e as salas em si
+// aqui vou armazenar os estados de jogo da sala, funções dos jogadores,
+// se os jogadores ja estão prontos(ready, not ready, morto e etc) 
+// pra pular de fase e etc
+
+// Por enquanto só tem a inicialização do sqlite, 
+// os dados em si tão sendo armazenados na variavel Rooms logo abaixo
+// TODO: Decidir se vai continuar com a variavel ou com o sqlite
+exports.initRooms = () => {
+    const db = await open({
+        filename: ':memory:',
+        driver: sqlite3.Database
+    });
+
+    await db.exec(`
+        CREATE TABLE salas (
+            id TEXT PRIMARY KEY,
+            dono TEXT,
+            status TEXT
+        )
+    `);
+
+    await db.exec(`
+        CREATE TABLE jogadores_da_sala (
+            id TEXT PRIMARY KEY,
+            nome TEXT,
+            status TEXT,
+            funcao TEXT
+        )
+    `);
+}
+
 var Rooms = {}
 
+
+// Cria um código de 6 caracteres
 exports.GetRandomCode = () => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     var code = ''
@@ -9,14 +49,40 @@ exports.GetRandomCode = () => {
     return code
 }
 
-exports.CreateRoom = (socket) => {
+exports.CreateRoom = (jogador) => {
     const code = GetRandomCode()
     //Placeholder
     Rooms[code] = {
-        "dono": socket.id,//? talvez(?)
+        "sala_id": code,
+        "dono": jogador.id,//? talvez(?)
         "estado":"noite",
-        "jogadores":[],
+        "jogadores":{},
         "privacidade":"publico"
     }
     return Rooms[code]
+}
+
+exports.JoinRoom = (socket, code) => {
+    const room = Rooms[code]
+    if(!room){
+        console.log(socket.id + " tentou entrar em uma sala inexistente")
+        return "Erro: sala não encontrada" 
+    }
+
+    const jogadorExiste = room.jogadores[socket.id]
+    if(jogadorExiste){
+        console.log(socket.id + " ja existe na sala: " + code)
+        return "Erro: jogador ja existe na sala"
+    }
+
+    Rooms[code].jogadores[socket.id] = {
+        "socket_id":socket.id,
+        "pronto": false
+    }
+    socket.join(code)
+    
+}
+
+exports.ChangeState = () => {
+
 }
